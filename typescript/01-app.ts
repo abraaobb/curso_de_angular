ba955cd9b6eb4d8b64f942b9702b7bc77848dc87
Abraão Brito <abraaobritof10@gmail.com>
2021-06-28 09:26:51 -0400

    funções

---
+++ typescript/01-app.ts
@@ -0,0 +1,11 @@
+let message: string = "Help me, Obi-Wan Kenobi. You're me only hope!"
+console.log(message)
+
+let episode: number = 4
+console.log("This is episode " + episode)
+episode = episode + 1
+console.log("Next episode is " + episode)
+
+let favoriteDroid
+favoriteDroid = 'BB-8'
+console.log("My favorite droid is " + favoriteDroid)
